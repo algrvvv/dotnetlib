9a12f0e1e61fdbab47e3bfb08a440edc6fb9bb15
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2025-05-28 16:08:49 +0300

    добавлены миграции

---
+++ Migrations/20250523142505_Init Migration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace lib.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "authors",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    fullname = table.Column<string>(name: "full_name", type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_authors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    pagecount = table.Column<int>(name: "page_count", type: "integer", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    authorid = table.Column<int>(name: "author_id", type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_books_authors_author_id",
+                        column: x => x.authorid,
+                        principalTable: "authors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_books_author_id",
+                table: "books",
+                column: "author_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "authors");
+        }
+    }
+}
