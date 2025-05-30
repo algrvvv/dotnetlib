c943fcdafb8ccc60368824f4617b0fa79d59d3ad
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2025-05-30 10:00:00 +0300

    реализован вывод конкретной книги + общий список

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,21 +1,63 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using lib.Models;
+using lib.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace lib.Controllers;
 
 public class BooksController : Controller
 {
     private readonly ILogger<BooksController> _logger;
+    private readonly LibContext _context;
 
-    public BooksController(ILogger<BooksController> logger)
+    public BooksController(ILogger<BooksController> logger, LibContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Show()
+    /// <summary>
+    /// получение всех книг и их вывод
+    /// </summary>
+    public async Task<IActionResult> Index()
     {
-        return View();
+        try
+        {
+            var books = await _context.Books
+                .Include(b => b.Author)
+                .ToListAsync();
+
+            _logger.LogInformation("got books for index page. books count: {Count}", books.Count);
+            return View(books);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "failed to get books for index page");
+            TempData["Error"] = "Что то пошло не так";
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+    public async Task<IActionResult> Show(int id)
+    {
+        try
+        {
+            var book = await _context.Books
+                .Include(b => b.Author)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            _logger.LogInformation("");
+            return View(book);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "failed to get data for show book info");
+            TempData["Error"] = "Что то пошло не так";
+            return RedirectToAction("Index");
+        }
+    }
+
     }
 
     public IActionResult Edit(int id)
